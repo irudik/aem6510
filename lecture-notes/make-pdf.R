c6eb94f8e4e6766bc18208a9dac2836765d7bb66
Ivan Rudik <irudik@cornell.edu>
2021-11-04 10:53:19 -0400

    add ln11 pdf

--- lecture-notes/make-pdf.R
+++ lecture-notes/make-pdf.R
@@ -1,7 +1,7 @@
 # Makes pdf slides for all lectures
 oldwd <- getwd()
 setwd("~/Desktop/git/aem6510/lecture-notes/")
-lapply(list.files(pattern = "*.html", recursive = T)[10], 
+lapply(list.files(pattern = "*.html", recursive = T)[11], 
        function(file) {
          xaringan::decktape(
            file, 
